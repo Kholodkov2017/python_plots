c35b59acb62e7e234f467bee124145481f2c67bd
vholodkov <vholodkov@zuzex.com>
2023-03-22 23:52:18 +0300

    add percentage to pie chart

--- plot_scripty.py
+++ plot_scripty.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.axis as ax
 
+# подключение кб бд
 conn = pyodbc.connect('Driver={SQL Server};'
                     'Server=DESKTOP-656KKVV\SQLEXPRESS01;'
                     'Database=zdb;'
                     'Trusted_Connection=yes;')
-sql = 'Select Data.year as Год, SUM(Data.pk) as ОбщаяЧисленностьСтудентов from Data group by Data.year ORDER BY Data.Year'
 
+
+sql = 'Select Data.year as Год, SUM(Data.pk) as ОбщаяЧисленностьСтудентов from Data group by Data.year ORDER BY Data.Year'
 plt.figure(1)
 # fig, axs = plt.subplots(nrows= 3 , ncols= 1 )
 data = pd.read_sql(sql,conn)
@@ -33,7 +35,7 @@ plt.xlabel('Федеральный округ')
 plt.figure(3)
 sql3 = "select Data.region_name [Регион],  sum(Data.rnd) [Объем НИОКР] from Data where Data.federal_district_short = 'ПФО' and year = 2016 group by Data.region_name"
 data3 =  pd.read_sql(sql3,conn)
-plt.pie(data3['Объем НИОКР'], labels=data3['Регион'])
+plt.pie(data3['Объем НИОКР'], labels=data3['Регион'], autopct='%1.1f%%')
 plt.title('Объем НИОКР по субъектам в Приволжском ФО за 2016 год')
 plt.show()
 
